921eb419610d81dd799b1fe6cdb111ec523cf6bc
Ofri Kleinfeld <ofri64@gmail.com>
2020-10-19 15:49:38 +0300

    first half of the assignment

--- src/main/java/p1/IPDataAnalytics.java
+++ src/main/java/p1/IPDataAnalytics.java
@@ -5,10 +5,10 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.api.java.tuple.Tuple3;
 
 
 public class IPDataAnalytics {
@@ -25,19 +25,46 @@ public class IPDataAnalytics {
         env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
 
         DataStream<String> data = env.readTextFile(params.get("input"));
-        DataStream<Tuple2<String, Long>> websiteClicks =
+        KeyedStream<Tuple2<String, Long>, String> websiteClicks =
                 data.map(value -> new Tuple2<>(value.split(",")[4], 1L))
+                .returns(new TypeHint<Tuple2<String, Long>>() {})
+                .keyBy(value -> value.f0);
+
+        SingleOutputStreamOperator<Tuple2<String, Long>> websiteTotalClicks = websiteClicks.sum(1);
+        SingleOutputStreamOperator<Tuple2<String, Long>> mostClickedWebsite = websiteTotalClicks.keyBy(value -> "all").maxBy(1);
+        SingleOutputStreamOperator<Tuple2<String, Long>> leastClickedWebsite = websiteTotalClicks.keyBy(value -> "all").minBy(1);
+
+        SingleOutputStreamOperator<Tuple3<String, String, Long>> websiteUsersSum =
+                data.map(value -> {
+                    String[] row = value.split(",");
+                    return new Tuple3<>(row[4], row[0], 1L);
+                })
+                .returns(new TypeHint<Tuple3<String, String, Long>>() {})
+                .keyBy(value -> value.f0 + "_" + value.f1)
+                .sum(2);
+
+
+
+
+        SingleOutputStreamOperator<Tuple2<String, Long>> websiteDistinctUsers =
+                data.map(value -> {
+                    String[] row = value.split(",");
+                    return new Tuple3<>(row[4], row[0], 1L);
+                })
+                .returns(new TypeHint<Tuple3<String, String, Long>>() {})
+                .keyBy(value -> value.f0 + "_" + value.f1)
+                .sum(2)
+                .map(value -> new Tuple2<>(value.f0, 1L))
                 .returns(new TypeHint<Tuple2<String, Long>>() {})
                 .keyBy(value -> value.f0)
                 .sum(1);
 
-        KeyedStream<Long, String> websiteCl2ick21212s =
-                data.map(value -> new Tuple2<>(value.split(",")[4], 1L))
-                .returns(new TypeHint<Tuple2<String, Long>>() {})
-                .keyBy(value -> value.f0);
-//                .maxBy(1);
 
-        websiteClicks.writeAsText(params.get("output1"));
+//        websiteTotalClicks.writeAsText(params.get("output1"));
+//        mostClickedWebsite.writeAsText(params.get("output2"));
+//        leastClickedWebsite.writeAsText(params.get("output3"));
+        websiteUsersSum.writeAsText(params.get("output1"));
+        websiteDistinctUsers.writeAsText(params.get("output2"));
         env.execute("IP Analytics Assignment 2");
     }
 
